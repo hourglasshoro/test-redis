5593dbe74084e23e4c6687c9d01ff7f16cf308a0
nakajimaibuki <nakajimai@crunchtimer.jp>
2020-08-28 15:47:20 +0900

    update: schema変更

--- mysql/initdb/schema.sql
+++ mysql/initdb/schema.sql
@@ -4,8 +4,9 @@ CREATE TABLE IF NOT EXISTS messages
     body       VARCHAR(10000) NOT NULL,
     user_id    MEDIUMINT      NOT NULL,
     channel_id MEDIUMINT      NOT NULL,
+    redis_id   VARCHAR(36)    NOT NULL,
     type       VARCHAR(10)    NOT NULL,
-    created_at TIMESTAMP      NOT NULL
+    created_at VARCHAR(20)    NOT NULL
 --      FOREIGN KEY(user_id) REFERENCES users(user_id),
 --      FOREIGN KEY(channel_id) REFERENCES channels(channel_id)
 );
